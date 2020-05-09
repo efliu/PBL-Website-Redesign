2b20460f7859000f1df4ebb97de1cd1aa76c670d
Emily Lu <e.lu@berkeley.edu>
2020-05-08 23:04:42 -0700

    fixed leadership containers

---
+++ js/hover-text- color.js
@@ -0,0 +1,6 @@
+function highlightBG() {  
+    document.getElementById('element').className='AttachOnMouseOverText';   
+}
+function highlightOutBG(){
+    document.getElementById('element').className='AttachOnMouseOutText';
+}
